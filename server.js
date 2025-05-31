09aaa4724c92dbe6b630fcd5a4c90e28d3e50e84
Raunak Bhuyan <raunakbhuyan@gmail.com>
2025-05-31 14:19:18 +0530

    Initial commit

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ connectDB();
 
 const app = express()
 
+//middlewares
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
@@ -21,13 +22,6 @@ app.use('/api/v1/users', require('./routes/userRoute'));
 
 //transaction routes
 app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
-
-//static files
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 //port
 const PORT = 8080 || process.env.PORT
